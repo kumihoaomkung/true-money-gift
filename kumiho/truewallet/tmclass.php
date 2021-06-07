49cd00309a01446e6beed0b1fc4b10b90ab1d982
Kumiho's Aom <59776731+kumihoaomkung@users.noreply.github.com>
2021-06-07 21:27:39 +0700

    Update tmclass.php

--- kumiho/truewallet/tmclass.php
+++ kumiho/truewallet/tmclass.php
@@ -115,19 +115,3 @@ if(isset($_POST['link'])){
     $request = $tm->VoucherCode('0645091974', $_POST['link'], true);
     print_r($request);
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <input type="text" name="link" id="link" placeholder="ใส่ลิงค์" require>
-        <button type="submit">เติม</button>
-    </form>
-</body>
-</html>
